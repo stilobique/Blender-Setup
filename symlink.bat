12889995c1b3707952e35b2dd4300ed8c6662272
Aurélien VAILLANT <aurelienv@Backlight.local>
2018-11-30 11:04:19 +0100

    Update the symlink bat, add character to work with space folder

--- symlink.bat
+++ symlink.bat
@@ -5,6 +5,6 @@ echo "Path like %APPDATA%\Roaming\Blender Foundation\Blender\2.XX\scripts\addons
 set /p path_blender="Blender Scripts folder : " 
 
 REM for /d %%d in (*.*) do (echo Cible %path_blender%\%%d Lien %CD%\%%d)
-for /d %%d in (*.*) do (MKLINK /D %path_blender%\%%d %CD%\%%d)
+for /d %%d in (*.*) do (MKLINK /D "%path_blender%\%%d" "%CD%\%%d")
 
 pause
