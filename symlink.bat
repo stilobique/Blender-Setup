13270775dd0b9662fdce0ccdd659f0f67596f28d
stilobique <aurelienv@backlight.fr>
2018-12-04 12:07:40 +0100

    Fix \ on a path

--- symlink.bat
+++ symlink.bat
@@ -4,7 +4,7 @@
 :: Select your Blender version
 set path_blender=%APPDATA%\Roaming\Blender Foundation\Blender
 set /p version="Blender Version Used (2.xx) : " 
-set subfolder=\scripts\addons
+set subfolder=scripts\addons
 
 REM for /d %%d in (*.*) do (echo Cible %path_blender%\%%d Lien %CD%\%%d)
 for /d %%d in (*.*) do (MKLINK /D "%path_blender%\%version%\%subfolder%\%%d" "%CD%\%%d")
