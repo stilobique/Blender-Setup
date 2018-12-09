ab2f05b8629463a2d624bddeca70e5c0b2d88a3a
stilobique <aurelienvlt@free.fr>
2018-12-10 00:20:00 +0100

    Fix path symlink

--- symlink.bat
+++ symlink.bat
@@ -1,10 +1,13 @@
 :: Make a loop with all folder to generate a sym link with blender
 @echo off
+cd blender-addons
 
 :: Select your Blender version
-set path_blender=%APPDATA%\Roaming\Blender Foundation\Blender
+set path_blender=%APPDATA%\Blender Foundation\Blender
 set /p version="Blender Version Used (2.xx) : " 
 set subfolder=scripts\addons
 
 REM for /d %%d in (*.*) do (echo Cible %path_blender%\%%d Lien %CD%\%%d)
 for /d %%d in (*.*) do (MKLINK /D "%path_blender%\%version%\%subfolder%\%%d" "%CD%\%%d")
+
+cd ..
