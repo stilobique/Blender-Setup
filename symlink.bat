8e41925285ea0d61e1d42c4b1ff842cf845ad0e7
stilobique <aurelienvlt@free.fr>
2018-12-10 00:33:40 +0100

    Update the symlink bat

--- symlink.bat
+++ symlink.bat
@@ -5,9 +5,17 @@ cd blender-addons
 :: Select your Blender version
 set path_blender=%APPDATA%\Blender Foundation\Blender
 set /p version="Blender Version Used (2.xx) : " 
-set subfolder=scripts\addons
 
-REM for /d %%d in (*.*) do (echo Cible %path_blender%\%%d Lien %CD%\%%d)
+:: Test path before the symlink creation
+set TestFolder1=%path_blender%\%version%\scripts\
+if not exist "%TestFolder1%" mkdir "%TestFolder1%"
+
+set TestFolder2=%TestFolder1%\addons\
+if not exist "%TestFolder2%" mkdir "%TestFolder2%"
+
+:: Generate all Symlink folder
+set subfolder=scripts\addons
+for /d %%d in (*.*) do (echo Cible %path_blender%\%%d Lien %CD%\%%d)
 for /d %%d in (*.*) do (MKLINK /D "%path_blender%\%version%\%subfolder%\%%d" "%CD%\%%d")
 
 cd ..
