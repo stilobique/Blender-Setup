6ca20e1580c945df0c483400dcb65da3055daebb
stilobique <aurelienv@backlight.fr>
2018-12-12 10:49:08 +0100

    Update install process, fix path and add the preset install

--- install_addons.bat
+++ install_addons.bat
@@ -2,12 +2,8 @@
 @echo off
 cd blender-addons
 
-:: Select your Blender version
-set path_blender=%APPDATA%\Blender Foundation\Blender
-set /p version="Blender Version Used (2.xx) : " 
-
 :: Test path before the symlink creation
-set TestFolder1=%path_blender%\%version%\scripts\
+set TestFolder1=%blender_path%\%version%\scripts\
 if not exist "%TestFolder1%" mkdir "%TestFolder1%"
 
 set TestFolder2=%TestFolder1%\addons\
@@ -15,7 +11,6 @@ if not exist "%TestFolder2%" mkdir "%TestFolder2%"
 
 :: Generate all Symlink folder
 set subfolder=scripts\addons
-for /d %%d in (*.*) do (echo Cible %path_blender%\%%d Lien %CD%\%%d)
-for /d %%d in (*.*) do (MKLINK /D "%path_blender%\%version%\%subfolder%\%%d" "%CD%\%%d")
+for /d %%d in (*.*) do (MKLINK /D "%blender_path%\%version%\%subfolder%\%%d" "%CD%\%%d")
 
 cd ..
