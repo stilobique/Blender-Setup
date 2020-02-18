330d95ee1ab8148168f0ee718b8e76f32a122a91
Aurelien Vaillant <aurelienvlt@free.fr>
2020-02-18 17:49:26 +0100

    Fix config setup install

--- setup_config.py
+++ setup_config.py
@@ -6,9 +6,11 @@ def config_symlink(bld_version):
     src_folder = join(dirname(abspath(__file__)), 'blender-config')
     dst_folder = join(getenv('APPDATA'), 'Blender Foundation', 'Blender', bld_version)
     try:
+        config_folder = join(dst_folder, 'config')
         if not isdir(dst_folder):
             makedirs(dst_folder)
-        symlink(src_folder, join(dst_folder, 'config'))
+        if not isdir(config_folder):
+            symlink(src_folder, config_folder)
         return True
-    except TypeError or FileNotFoundError:
+    except TypeError or FileNotFoundError or FileExistsError:
         return False
